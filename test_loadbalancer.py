df249c28d2856900e2eae9a533e47f33f99151c0
dudick123 <brian.dudick@outlook.com>
2022-05-15 19:00:25 -0400

    through config

--- test_loadbalancer.py
+++ test_loadbalancer.py
@@ -12,6 +12,7 @@ def client():
 def test_host_routing_mango(client):
     result = client.get('/', headers={'Host': 'www.mango.com'})
     assert b'This is the mango application.' in result.data
+    assert 200 == result.status_code
 
 
 def test_host_routing_apple(client):
@@ -21,5 +22,20 @@ def test_host_routing_apple(client):
 
 def test_host_routing_notfound(client):
     result = client.get('/', headers={'Host': 'www.notmango.com'})
+    assert b'Not Found' in result.data
+    assert 404 == result.status_code
+
+def test_path_routing_mango(client):
+    result = client.get('/mango')
+    assert b'This is the mango application.' in result.data
+
+
+def test_path_routing_apple(client):
+    result = client.get('/apple')
+    assert b'This is the apple application.' in result.data
+
+
+def test_path_routing_notfound(client):
+    result = client.get('/notmango')
     assert b'Not Found' in result.data
     assert 404 == result.status_code
